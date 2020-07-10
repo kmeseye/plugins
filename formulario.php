132087dae9cfb35f23f201b4ded2b0c7e10a73d2
Sergio <sergioval16@outlook.com>
2020-07-10 17:57:25 +0200

    añadir elementos ocultos

--- formulario.php
+++ formulario.php
@@ -1,22 +1,22 @@
 <?php
 /**
-* Plugin Name: formulario
-* Plugin URI: https://formulario.com/
-* Description: new formulario pluging.
-* Author: sergio
-* Author URI: https://automattic.com/wordpress-plugins/
-* License: GPLv2 or later
-* Text Domain: formulario
-*/
+ * Plugin Name: formulario
+ * Plugin URI: https://formulario.com/
+ * Description: new formulario pluging.
+ * Author: sergio
+ * Author URI: https://automattic.com/wordpress-plugins/
+ * License: GPLv2 or later
+ * Text Domain: formulario
+ */
 // Cuando el plugin se active se crea la tabla para recoger los datos si no existe
 register_activation_hook(__FILE__, 'Kfp_Aspirante_init');
- 
+
 /**
  * Crea la tabla para recoger los datos del formulario
  *
  * @return void
  */
-function Kfp_Aspirante_init() 
+function Kfp_Aspirante_init()
 {
     global $wpdb; // Este objeto global permite acceder a la base de datos de WP
     // Crea la tabla sólo si no existe
@@ -43,52 +43,54 @@ function Kfp_Aspirante_init()
 }
 //Define el shortcode y lo asocia a una función
 add_shortcode('kfp_aspirante_form', 'Kfp_Aspirante_form');
- 
-function Kfp_Aspirante_form() 
 
+function Kfp_Aspirante_form()
 {
     // Carga esta hoja de estilo para poner más bonito el formulario
-wp_enqueue_style('css_aspirante', plugins_url('style.css', __FILE__));
+    wp_enqueue_style('css_aspirante', plugins_url('style.css', __FILE__));
 
-global $wpdb; // Este objeto global permite acceder a la base de datos de WP
-// Si viene del formulario  graba en la base de datos
-// Cuidado con el último igual de la condición del if que es doble
-if ($_POST['nombre'] != ''
-    AND is_email($_POST['correo'])
-    AND $_POST['nivel_html'] != ''
-    AND $_POST['nivel_css'] != ''
-    AND $_POST['nivel_js'] != ''      
-    AND $_POST['aceptacion'] == '1'
-) {
-    $tabla_aspirantes = $wpdb->prefix . 'aspirante'; 
-    $nombre = sanitize_text_field($_POST['nombre']);
-    $correo = $_POST['correo'];
-    $nivel_html = (int)$_POST['nivel_html'];
-    $nivel_css = (int)$_POST['nivel_css'];
-    $nivel_js = (int)$_POST['nivel_js'];
-    $aceptacion = (int)$_POST['aceptacion'];
-    $created_at = date('Y-m-d H:i:s');
-    $wpdb->insert(
-        $tabla_aspirantes,
-        array(
-            'nombre' => $nombre,
-            'correo' => $correo,
-            'nivel_html' => $nivel_html,
-            'nivel_css' => $nivel_css,
-            'nivel_js' => $nivel_js,
-            'aceptacion' => $aceptacion,
-            'created_at' => $created_at,
-        )
-    );
-    echo "<p class='exito'><b>Tus datos han sido registrados</b>. Gracias 
+    global $wpdb; // Este objeto global permite acceder a la base de datos de WP
+    // Si viene del formulario  graba en la base de datos
+    // Cuidado con el último igual de la condición del if que es doble
+    if ($_POST['nombre'] != ''
+        and is_email($_POST['correo'])
+        and $_POST['nivel_html'] != ''
+        and $_POST['nivel_css'] != ''
+        and $_POST['nivel_js'] != ''
+        and $_POST['aceptacion'] == '1'
+        AND wp_verify_nonce($_POST['aspirante_nonce'], 'graba_aspirante')
+
+    ) {
+        $tabla_aspirantes = $wpdb->prefix . 'aspirante';
+        $nombre = sanitize_text_field($_POST['nombre']);
+        $correo = $_POST['correo'];
+        $nivel_html = (int) $_POST['nivel_html'];
+        $nivel_css = (int) $_POST['nivel_css'];
+        $nivel_js = (int) $_POST['nivel_js'];
+        $aceptacion = (int) $_POST['aceptacion'];
+        $created_at = date('Y-m-d H:i:s');
+        $wpdb->insert(
+            $tabla_aspirantes,
+            array(
+                'nombre' => $nombre,
+                'correo' => $correo,
+                'nivel_html' => $nivel_html,
+                'nivel_css' => $nivel_css,
+                'nivel_js' => $nivel_js,
+                'aceptacion' => $aceptacion,
+                'created_at' => $created_at,
+            )
+        );
+        echo "<p class='exito'><b>Tus datos han sido registrados</b>. Gracias
         por tu interés. En breve contactaré contigo.<p>";
-}
+    }
     // Esta función de PHP activa el almacenamiento en búfer de salida (output buffer)
     // Cuando termine el formulario lo imprime con la función ob_get_clean
     ob_start();
     ?>
-    <form action="<?php get_the_permalink(); ?>" method="post" id="form_aspirante"
+    <form action="<?php get_the_permalink();?>" method="post" id="form_aspirante"
 class="cuestionario">
+    <?php wp_nonce_field('graba_aspirante', 'aspirante_nonce');?>
         <div class="form-input">
             <label for="nombre">Nombre</label>
             <input type="text" name="nombre" id="nombre" required>
@@ -100,35 +102,35 @@ class="cuestionario">
         <div class="form-input">
             <label for="nivel_html">¿Cuál es tu nivel de HTML?</label>
             <input type="radio" name="nivel_html" value="1" required> Nada
-            <br><input type="radio" name="nivel_html" value="2" required> Estoy 
+            <br><input type="radio" name="nivel_html" value="2" required> Estoy
                 aprendiendo
-            <br><input type="radio" name="nivel_html" value="3" required> Tengo 
+            <br><input type="radio" name="nivel_html" value="3" required> Tengo
                 experiencia
-            <br><input type="radio" name="nivel_html" value="4" required> Lo 
+            <br><input type="radio" name="nivel_html" value="4" required> Lo
                 domino al dedillo
         </div>
         <div class="form-input">
             <label for="nivel_css">¿Cuál es tu nivel de CSS?</label>
             <input type="radio" name="nivel_css" value="1" required> Nada
-            <br><input type="radio" name="nivel_css" value="2" required> Estoy 
+            <br><input type="radio" name="nivel_css" value="2" required> Estoy
                 aprendiendo
-            <br><input type="radio" name="nivel_css" value="3" required> Tengo 
+            <br><input type="radio" name="nivel_css" value="3" required> Tengo
                 experiencia
-            <br><input type="radio" name="nivel_css" value="4" required> Lo 
+            <br><input type="radio" name="nivel_css" value="4" required> Lo
                 domino al dedillo
         </div>
         <div class="form-input">
             <label for="nivel_js">¿Cuál es tu nivel de JavaScript?</label>
             <input type="radio" name="nivel_js" value="1" required> Nada
-            <br><input type="radio" name="nivel_js" value="2" required> Estoy 
+            <br><input type="radio" name="nivel_js" value="2" required> Estoy
                 aprendiendo
-            <br><input type="radio" name="nivel_js" value="3" required> Tengo 
+            <br><input type="radio" name="nivel_js" value="3" required> Tengo
                 experiencia
-            <br><input type="radio" name="nivel_js" value="4" required> Lo domino 
+            <br><input type="radio" name="nivel_js" value="4" required> Lo domino
 al dedillo
         </div>
         <div class="form-input">
-            <label for="aceptacion">La información facilitada se tratará 
+            <label for="aceptacion">La información facilitada se tratará
             con respeto y admiración.</label>
             <input type="checkbox" id="aceptacion" name="aceptacion"
 value="1" required> Entiendo y acepto las condiciones
@@ -138,7 +140,7 @@ value="1" required> Entiendo y acepto las condiciones
         </div>
     </form>
     <?php
-     
+
     // Devuelve el contenido del buffer de salida
     return ob_get_clean();
 }
