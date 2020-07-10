341d8d18fe96933161ae497cbcd17887a8a1ac5d
Sergio <sergioval16@outlook.com>
2020-07-10 16:41:56 +0200

    add pluggin

---
+++ formulario.php
@@ -0,0 +1,79 @@
+<?php
+/**
+* Plugin Name: formulario
+
+
+Plugin Name: formulario
+Plugin URI: https://akismet.com/
+Description: Used by millions, Akismet is quite possibly the best way in the world to <strong>protect your blog from spam</strong>. It keeps your site protected even while you sleep. To get started: activate the Akismet plugin and then go to your Akismet Settings page to set up your API key.
+Version: 4.1.5
+Author: Automattic
+Author URI: https://automattic.com/wordpress-plugins/
+License: GPLv2 or later
+Text Domain: akismet
+*/
+
+//Define el shortcode y lo asocia a una función
+add_shortcode('kfp_aspirante_form', 'Kfp_Aspirante_form');
+ 
+function Kfp_Aspirante_form() 
+{
+    // Esta función de PHP activa el almacenamiento en búfer de salida (output buffer)
+    // Cuando termine el formulario lo imprime con la función ob_get_clean
+    ob_start();
+    ?>
+    <form action="<?php get_the_permalink(); ?>" method="post" id="form_aspirante
+class="cuestionario"">
+        <div class="form-input">
+            <label for="nombre">Nombre</label>
+            <input type="text" name="nombre" id="nombre" required>
+        </div>
+        <div class="form-input">
+            <label for='correo'>Correo</label>
+            <input type="email" name="correo" id="correo" required>
+        </div>
+        <div class="form-input">
+            <label for="nivel_html">¿Cuál es tu nivel de HTML?</label>
+            <input type="radio" name="nivel_html" value="1" required> Nada
+            <br><input type="radio" name="nivel_html" value="2" required> Estoy 
+                aprendiendo
+            <br><input type="radio" name="nivel_html" value="3" required> Tengo 
+                experiencia
+            <br><input type="radio" name="nivel_html" value="4" required> Lo 
+                domino al dedillo
+        </div>
+        <div class="form-input">
+            <label for="nivel_css">¿Cuál es tu nivel de CSS?</label>
+            <input type="radio" name="nivel_css" value="1" required> Nada
+            <br><input type="radio" name="nivel_css" value="2" required> Estoy 
+                aprendiendo
+            <br><input type="radio" name="nivel_css" value="3" required> Tengo 
+                experiencia
+            <br><input type="radio" name="nivel_css" value="4" required> Lo 
+                domino al dedillo
+        </div>
+        <div class="form-input">
+            <label for="nivel_js">¿Cuál es tu nivel de JavaScript?</label>
+            <input type="radio" name="nivel_js" value="1" required> Nada
+            <br><input type="radio" name="nivel_js" value="2" required> Estoy 
+                aprendiendo
+            <br><input type="radio" name="nivel_js" value="3" required> Tengo 
+                experiencia
+            <br><input type="radio" name="nivel_js" value="4" required> Lo domino 
+al dedillo
+        </div>
+        <div class="form-input">
+            <label for="aceptacion">La información facilitada se tratará 
+            con respeto y admiración.</label>
+            <input type="checkbox" id="aceptacion" name="aceptacion"
+value="1" required> Entiendo y acepto las condiciones
+        </div>
+        <div class="form-input">
+            <input type="submit" value="Enviar">
+        </div>
+    </form>
+    <?php
+     
+    // Devuelve el contenido del buffer de salida
+    return ob_get_clean();
+}
